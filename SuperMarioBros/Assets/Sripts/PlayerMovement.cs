3bce05478c704dfbe5d839ef462553e4e40e1a3e
DRyu1412 <thanhduy8847@gmail.com>
2023-05-03 01:18:49 +0700

    add jumping and gravity for mario

--- SuperMarioBros/Assets/Sripts/PlayerMovement.cs
+++ SuperMarioBros/Assets/Sripts/PlayerMovement.cs
@@ -9,6 +9,14 @@ public class PlayerMovement : MonoBehaviour
     private float inputAxis;
 
     public float movespeed = 8;
+    public float maxJumpHeight = 5f;
+    public float maxJumpTime = 1f;
+
+    public float jumpForce => (2f * maxJumpHeight) / (maxJumpTime / 2);
+    public float gravity => (-2f * maxJumpHeight) / Mathf.Pow((maxJumpTime / 2), 2);
+
+    public bool grounded { get; private set; }
+    public bool jumping { get; private set; }
 
     private void Awake()
     {
@@ -19,6 +27,14 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         HorizontalMovement();
+        grounded = rigidbody.Raycast(Vector2.down);
+
+        if (grounded)
+        {
+            GroundedMovement();
+        }
+
+        ApplyGravity();
     }
 
     private void FixedUpdate()
@@ -36,6 +52,27 @@ public class PlayerMovement : MonoBehaviour
     {
         inputAxis = Input.GetAxis("Horizontal");
         velocity.x = Mathf.MoveTowards(velocity.x, inputAxis * movespeed, movespeed * Time.deltaTime);
+    }
+
+    public void GroundedMovement()
+    {
+        velocity.y = Mathf.Max(velocity.y, 0f);
+        jumping = velocity.y > 0f;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            velocity.y = jumpForce;
+            jumping = true;
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        bool falling = velocity.y < 0f || !Input.GetButton("Jump");
+        float multiplier = falling ? 2f : 1f;  
+
+        velocity.y += gravity * multiplier * Time.deltaTime;
+        velocity.y = Mathf.Max(velocity.y, gravity / 2);
     }
 
     
