31f5f5ab10b003d0fec629497d3d3baed73c5f51
DRyu1412 <thanhduy8847@gmail.com>
2023-05-13 21:48:33 +0700

    add script for exit pipe
    change position of mario and camera when go underground

--- SuperMarioBros/Assets/Scripts/SideScrolling.cs
+++ SuperMarioBros/Assets/Scripts/SideScrolling.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class SideScrolling : MonoBehaviour
 {
     private Transform player;
+    public float height = 6.5f;
+    public float undergroundHeight = -9.5f;
 
     private void Awake()
     {
@@ -13,6 +15,13 @@ public class SideScrolling : MonoBehaviour
     {
         Vector3 cameraPosition = transform.position;
         cameraPosition.x = Mathf.Max(cameraPosition.x, player.position.x);
+        transform.position = cameraPosition;
+    }
+
+    public void SetUnderground(bool underground)
+    {
+        Vector3 cameraPosition = transform.position;
+        cameraPosition.y = underground ? undergroundHeight : height;
         transform.position = cameraPosition;
     }
 }
